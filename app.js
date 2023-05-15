35c93b32db8f7c00b8cdcb63bc22b58cb2770976
JenThomasJames <jenthomasjames123@gmail.com>
2023-05-15 22:47:32 +0530

    Feat: Add download route in app.js file

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const downloadRoute = require("./routes/download");
+
 const app = express();
 
 const PORT = 8000;
 
 app.use(bodyParser.json());
+
+app.use("/download", downloadRoute);
 
 app.listen(PORT, () => {
   console.log("inTunes API server running on localhost:" + PORT);
