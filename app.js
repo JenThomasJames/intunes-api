3f105f95112ab55873d92f65b56d60beceb4478b
JenThomasJames <jenthomasjames123@gmail.com>
2023-05-17 13:27:30 +0530

    Feat: Resolve CORS

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 
 const PORT = 8000;
 
+// Resolve CORS
+app.use((_, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use("/download", downloadRoute);
